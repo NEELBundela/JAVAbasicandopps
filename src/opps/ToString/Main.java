53023f1d8a43af815d2c57a1b2a6724f3400b8aa
NEELBundela <neelbundela1807@gmail.com>
2025-08-21 20:09:47 +0530

    Method ToString

---
+++ src/opps/ToString/Main.java
@@ -0,0 +1,9 @@
+package opps.ToString;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car=new Car("BMW","M5",2023,"Black");
+
+        System.out.println(car);  //for print a String message use toString Method
+    }
+}
