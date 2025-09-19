f5262316b2afeaab16caa99d6b8ab643e66f0396
NEELBundela <neelbundela1807@gmail.com>
2025-09-19 22:14:51 +0530

    Package

---
+++ src/MyPackages/Test.java
@@ -0,0 +1,8 @@
+package MyPackages;
+import MyPackages.MyClass;
+public class Test {
+    public static void main(String[] args) {
+        MyClass ob = new MyClass();
+        ob.display();
+    }
+}
---
+++ src/TestPackage.java
@@ -0,0 +1,7 @@
+import MyPackages.MyClass;
+public class TestPackage {
+    public static void main(String[] args) {
+        MyClass ob = new MyClass();
+        ob.display();
+    }
+}
